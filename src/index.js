28ac67902a30f881295ee5f423391a03b33f8500
niklusrook <nikolas.vardeberg1@gmail.com>
2025-03-25 19:34:28 +0100

    feat: init

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Goon = void 0;
-var goon_1 = require("./components/goon/goon");
-Object.defineProperty(exports, "Goon", { enumerable: true, get: function () { return __importDefault(goon_1).default; } });
--- src/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as Goon } from "./components/goon/goon"
